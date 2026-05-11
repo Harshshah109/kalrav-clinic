062ec78979a6472331a3763ce801ffdd81594128
Harshshah109 <harshshah7755@gmail.com>
2026-05-11 16:09:52 +0530

    upgraded payment management system

--- src/services/therapistService.js
+++ src/services/therapistService.js
@@ -3,14 +3,8 @@ import {
   addDoc,
   getDocs,
   doc,
-  updateDoc,
-  setDoc
+  updateDoc
 } from 'firebase/firestore'
-
-import {
-  getAuth,
-  createUserWithEmailAndPassword
-} from 'firebase/auth'
 
 import {
   db
@@ -26,51 +20,13 @@ const therapistRef =
 export const addTherapist =
   async (data) => {
 
-    let uid = null
-
-    if (
-      data.email &&
-      data.password
-    ) {
-
-      const auth =
-        getAuth()
-
-      const userCredential =
-        await createUserWithEmailAndPassword(
-          auth,
-          data.email,
-          data.password
-        )
-
-      const user =
-        userCredential.user
-
-      uid = user.uid
-
-      await setDoc(
-        doc(db, 'users', user.uid),
-        {
-          role:
-            data.systemRole || 'therapist',
-
-          email:
-            data.email
-        }
-      )
-    }
-
-    const therapistData = {
-      ...data,
-      uid
-    }
-
-    delete therapistData.password
-    delete therapistData.systemRole
-
     await addDoc(
       therapistRef,
-      therapistData
+      {
+        ...data,
+        createdAt:
+          new Date()
+      }
     )
   }
 
